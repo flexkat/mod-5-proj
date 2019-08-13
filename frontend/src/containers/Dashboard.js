e46b72608a5dfb50e95b9c95cc13ebe9f62e08c6
flexkat <catherinemaritz1@gmail.com>
2019-08-13 16:52:54 +0100

    refactored code for table

--- frontend/src/containers/Dashboard.js
+++ frontend/src/containers/Dashboard.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Icon, Table } from 'semantic-ui-react'
+import { getDate } from '../utils/medicines'
 
 
 class Dashboard extends React.Component {
+
+  checkWhichIconToRender = (med, date, time) => {
+    return med.history[date] ? med.history[date] && med.history[date].status[time] ? <Icon color='green' name='checkmark' size='large'/> : <Icon color='red' name='circle' size='large'/> : <Icon color='orange' name='circle' size='large'/>
+  }
+
   render() {
     const medicines = this.props.medicines
     const allDateKeys = this.props.medicines.map(med => Object.keys(med.history))
     const allDatesArray = [].concat.apply([], allDateKeys);
     const dates = [...new Set(allDatesArray)]
+    const currentDate = getDate();
 
     return(
       <div>
@@ -25,28 +32,26 @@ class Dashboard extends React.Component {
               <>
               <Table.Row>
                 <Table.Cell rowSpan='2'>{med.name}</Table.Cell>
-                {med.morning ? <Table.Cell>AM</Table.Cell> : <Table.Cell></Table.Cell>}
-                <Table.Cell textAlign='center'>
-                  {med.history['08/11/2019'] && med.history['08/11/2019'].status.AM ? <Icon color='green' name='checkmark' size='large'/> : <Icon color='red' name='circle' size='large'/>}
-                </Table.Cell>
-                <Table.Cell textAlign='center'>
-                  {med.history['08/12/2019'] && med.history['08/12/2019'].status.AM ? <Icon color='green' name='checkmark' size='large'/> : <Icon color='red' name='circle' size='large'/>}
-                </Table.Cell>
-                <Table.Cell textAlign='center'>
-                  {med.history['08/13/2019'] && med.history['08/13/2019'].status.AM ? <Icon color='green' name='checkmark' size='large'/> : <Icon color='red' name='circle' size='large'/>}
-                </Table.Cell>
+                {med.morning 
+                  ? <Table.Cell>AM</Table.Cell> 
+                  : <Table.Cell></Table.Cell>
+                }
+                {dates.map(date => 
+                  <Table.Cell textAlign="center">
+                    {this.checkWhichIconToRender(med, date, "AM")}
+                  </Table.Cell>)
+                }
               </Table.Row>
               <Table.Row>
-                {med.evening ? <Table.Cell>PM</Table.Cell> : <Table.Cell></Table.Cell>}
-                <Table.Cell textAlign='center'>
-                  {med.history['08/11/2019'] && med.history['08/11/2019'].status.PM ? <Icon color='green' name='checkmark' size='large'/> : <Icon color='red' name='circle' size='large'/>}
-                </Table.Cell>
-                <Table.Cell textAlign='center'>
-                  {med.history['08/12/2019'] && med.history['08/12/2019'].status.PM ? <Icon color='green' name='checkmark' size='large'/> : <Icon color='red' name='circle' size='large'/>}
-                </Table.Cell>
-                <Table.Cell textAlign='center'>
-                  {med.history['08/13/2019'] && med.history['08/13/2019'].status.PM ? <Icon color='green' name='checkmark' size='large'/> : <Icon color='red' name='circle' size='large'/>}
-                </Table.Cell>
+                {med.evening 
+                  ? <Table.Cell>PM</Table.Cell> 
+                  : <Table.Cell></Table.Cell>
+                }
+                {dates.map(date => 
+                  <Table.Cell textAlign="center">
+                    {this.checkWhichIconToRender(med, date, "PM")}
+                  </Table.Cell>)
+                }
               </Table.Row>
               </>
             ))}
